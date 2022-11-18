05a74e16c265b12e9ae21e3f58cab4ede47cbfa4
Ryan290401 <118518681+Ryan290401@users.noreply.github.com>
2022-11-18 17:02:48 +0800

    Delete sales_bonus.py

--- sales_bonus.py
+++
@@ -1,9 +0,0 @@
-sales = float(input("Enter sales: $ "))
-while sales >= 0:
-    if sales >= 1000:
-        bonus = sales * 0.15
-    else:
-        bonus = sales * 0.1
-    print(f'your bonus : $ {bonus}')
-    sales = float(input("Enter sales: $ "))
-print("Invalid Input")
