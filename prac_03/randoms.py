c71788bb9bee6a46df9a79d4fa561b57c6980b39
Yuhsiang <118518681+Ryan290401@users.noreply.github.com>
2022-11-27 15:33:16 +0800

    Add files via upload

---
+++ prac_03/randoms.py
@@ -0,0 +1,6 @@
+import random
+
+print(random.randint(5, 20))  # 12, minimum: 5, maximum: 19
+print(random.randrange(3, 10, 2))  # 9, minimum: 3, maximum: 9
+print(random.uniform(2.5, 5.5))  # 2.9688197780210874, minimum: 2.5, maximum: 5.4999...
+print(random.randint(1,101))
